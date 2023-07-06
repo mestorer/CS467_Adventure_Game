c597e9a8f831b81ba64d38b55a006df48c32c922
Michael Estorer <m.estorer@yahoo.com>
2023-07-05 21:27:31 -0400

    Corrected error on Player class

--- player.py
+++ player.py
@@ -3,5 +3,5 @@ from game_object import GameObject
 class Player(GameObject):
     def __init__(self, player_data):
         super().__init__(player_data['name'])
-        self.items = []
-        self.location = []
+        self.items = player_data["items"]
+        self.location = player_data["location"]
