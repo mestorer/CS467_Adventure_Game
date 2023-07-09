182e6f052f6a0e6e0d810121f8e000c6fa18473b
Michael Estorer <m.estorer@yahoo.com>
2023-07-08 20:05:58 -0400

    Changed Player 'items' attribute to 'inventory'

--- player.py
+++ player.py
@@ -4,5 +4,5 @@ class Player(GameObject):
     def __init__(self, file_name, player_data):
         super().__init__(file_name)
         self.name = player_data['name']
-        self.items = player_data["items"]
+        self.inventory = player_data["inventory"]
         self.location = player_data["location"]
